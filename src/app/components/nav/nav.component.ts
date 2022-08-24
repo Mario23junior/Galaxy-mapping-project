fe199bc3647db062cf4b019840de07f56ce981ec
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-08-24 04:41:06 -0300

    renderizando componente na area de navegação

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  private router:Router
+
+  constructor(router:Router) {
+    this.router = router
+   }
 
   ngOnInit(): void {
+    this.router.navigate(['home'])
   }
 
 }
