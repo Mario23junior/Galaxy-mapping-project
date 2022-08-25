1746c9568d8ab06810621fbb99280940697c7436
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-08-25 05:49:26 -0300

    Criando modelo de dados galaticos

---
+++ src/app/model/galaxias.ts
@@ -0,0 +1,20 @@
+export interface Galaxias {
+
+    id: Number
+    nome: String;
+    urlImg: String;
+    magnitude: Number;
+    declinacao: String;
+    distancia: String;
+    desvioVermelho: String;
+    descricao: String
+    rotacao: Number;
+    dimensoes: Number;
+    raio: Number;
+    IdadeEstimada: Number;
+    massa: Number;
+    estrelas: Number;
+    constelacao: String;
+    coordenadas: String;
+
+}
