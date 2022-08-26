a9e9e9664c324cf47f620e30a60d0f24e0815848
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-08-26 06:14:00 -0300

    Inserindo dados experimentais

--- src/app/components/galaxy-list/galaxy-list.component.ts
+++ src/app/components/galaxy-list/galaxy-list.component.ts
@@ -29,7 +29,20 @@ export class GalaxyListComponent implements OnInit {
       desvioVermelho: '−0,001', rotacao: 240.000, massa: 1.222,
       coordenadas: '233mk23', descricao: '2e32mmad,ceekkr', estrelas: 2, IdadeEstimada: 10011010,
       magnitude: 3.81, dimensoes: 232.323, urlImg: 'exemplimg', raio: 110.0
+    }, {
+      id: 1, nome: 'Andromeda', constelacao: 'Andromeda',
+      declinacao: '+41° 16′ 9″', distancia: '2.537.000',
+      desvioVermelho: '−0,001', rotacao: 240.000, massa: 1.222,
+      coordenadas: '233mk23', descricao: '2e32mmad,ceekkr', estrelas: 2, IdadeEstimada: 10011010,
+      magnitude: 3.81, dimensoes: 232.323, urlImg: 'exemplimg', raio: 110.0
+    }, {
+      id: 1, nome: 'Andromeda', constelacao: 'Andromeda',
+      declinacao: '+41° 16′ 9″', distancia: '2.537.000',
+      desvioVermelho: '−0,001', rotacao: 240.000, massa: 1.222,
+      coordenadas: '233mk23', descricao: '2e32mmad,ceekkr', estrelas: 2, IdadeEstimada: 10011010,
+      magnitude: 3.81, dimensoes: 232.323, urlImg: 'exemplimg', raio: 110.0
     }
+    
   ];
 
   displayedObserveColumns = ['nome', 'constelacao', 'dimensoes', 'distancia']
