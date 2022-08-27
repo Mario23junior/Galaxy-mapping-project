9f4105f233151952953786d739d716d56e079367
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-08-26 23:30:05 -0300

    Criando service de acesso HTTP e inserindo dados de teste

--- src/app/components/galaxy-list/galaxy-list.component.ts
+++ src/app/components/galaxy-list/galaxy-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Galaxias } from 'src/app/model/galaxias';
+import { GalaxyServiceService } from '../service/galaxy-service.service';
 
 @Component({
   selector: 'app-galaxy-list',
@@ -8,48 +9,16 @@ import { Galaxias } from 'src/app/model/galaxias';
 })
 export class GalaxyListComponent implements OnInit {
 
-
-  galaxias: Galaxias[] = [
-    {
-      id: 1, nome: 'Andromeda', constelacao: 'Andromeda',
-      declinacao: '+41° 16′ 9″', distancia: '2.537.000',
-      desvioVermelho: '−0,001', rotacao: 240.000, massa: 1.222,
-      coordenadas: '233mk23', descricao: '2e32mmad,ceekkr', estrelas: 2, IdadeEstimada: 10011010,
-      magnitude: 3.81, dimensoes: 232.323, urlImg: 'exemplimg', raio: 110.0
-    },
-    {
-      id: 1, nome: 'Andromeda', constelacao: 'Andromeda',
-      declinacao: '+41° 16′ 9″', distancia: '2.537.000',
-      desvioVermelho: '−0,001', rotacao: 240.000, massa: 1.222,
-      coordenadas: '233mk23', descricao: '2e32mmad,ceekkr', estrelas: 2, IdadeEstimada: 10011010,
-      magnitude: 3.81, dimensoes: 232.323, urlImg: 'exemplimg', raio: 110.0
-    }, {
-      id: 1, nome: 'Andromeda', constelacao: 'Andromeda',
-      declinacao: '+41° 16′ 9″', distancia: '2.537.000',
-      desvioVermelho: '−0,001', rotacao: 240.000, massa: 1.222,
-      coordenadas: '233mk23', descricao: '2e32mmad,ceekkr', estrelas: 2, IdadeEstimada: 10011010,
-      magnitude: 3.81, dimensoes: 232.323, urlImg: 'exemplimg', raio: 110.0
-    }, {
-      id: 1, nome: 'Andromeda', constelacao: 'Andromeda',
-      declinacao: '+41° 16′ 9″', distancia: '2.537.000',
-      desvioVermelho: '−0,001', rotacao: 240.000, massa: 1.222,
-      coordenadas: '233mk23', descricao: '2e32mmad,ceekkr', estrelas: 2, IdadeEstimada: 10011010,
-      magnitude: 3.81, dimensoes: 232.323, urlImg: 'exemplimg', raio: 110.0
-    }, {
-      id: 1, nome: 'Andromeda3333333333', constelacao: 'Andromeda',
-      declinacao: '+41° 16′ 9″', distancia: '2.537.000',
-      desvioVermelho: '−0,001', rotacao: 240.000, massa: 1.222,
-      coordenadas: '233mk23', descricao: '2e32mmad,ceekkr', estrelas: 2, IdadeEstimada: 10011010,
-      magnitude: 3.81, dimensoes: 232.323, urlImg: 'exemplimg', raio: 110.0
-    }
-    
-  ];
+  galaxias: Galaxias[] = []
+  galaxiService: GalaxyServiceService
 
   displayedObserveColumns = ['nome', 'constelacao', 'dimensoes', 'distancia']
   displayedCaracteColumns = ['raio', 'magnitude', 'massa', 'estrelas']
   displayedCSensorEspecColumns = ['rotacao', 'declinacao', 'IdadeEstimada', 'coordenadas']
 
   constructor() {
+    this.galaxiService = new GalaxyServiceService(); 
+    this.galaxias = this.galaxiService.listAll()
   }
 
   ngOnInit(): void {
---
+++ src/app/components/service/galaxy-service.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { Galaxias } from 'src/app/model/galaxias';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GalaxyServiceService {
+
+  constructor() { }
+
+  listAll ():Galaxias[] {
+    return [
+      {
+        id: 1, nome: 'Andromeda', constelacao: 'Andromeda',
+        declinacao: '+41° 16′ 9″', distancia: '2.537.000',
+        desvioVermelho: '−0,001', rotacao: 240.000, massa: 1.222,
+        coordenadas: '233mk23', descricao: '2e32mmad,ceekkr', estrelas: 2, IdadeEstimada: 10011010,
+        magnitude: 3.81, dimensoes: 232.323, urlImg: 'exemplimg', raio: 110.0
+      },
+      {
+        id: 1, nome: 'Andromeda', constelacao: 'Andromeda',
+        declinacao: '+41° 16′ 9″', distancia: '2.537.000',
+        desvioVermelho: '−0,001', rotacao: 240.000, massa: 1.222,
+        coordenadas: '233mk23', descricao: '2e32mmad,ceekkr', estrelas: 2, IdadeEstimada: 10011010,
+        magnitude: 3.81, dimensoes: 232.323, urlImg: 'exemplimg', raio: 110.0
+      }, {
+        id: 1, nome: 'Andromeda', constelacao: 'Andromeda',
+        declinacao: '+41° 16′ 9″', distancia: '2.537.000',
+        desvioVermelho: '−0,001', rotacao: 240.000, massa: 1.222,
+        coordenadas: '233mk23', descricao: '2e32mmad,ceekkr', estrelas: 2, IdadeEstimada: 10011010,
+        magnitude: 3.81, dimensoes: 232.323, urlImg: 'exemplimg', raio: 110.0
+      }, {
+        id: 1, nome: 'Andromeda', constelacao: 'Andromeda',
+        declinacao: '+41° 16′ 9″', distancia: '2.537.000',
+        desvioVermelho: '−0,001', rotacao: 240.000, massa: 1.222,
+        coordenadas: '233mk23', descricao: '2e32mmad,ceekkr', estrelas: 2, IdadeEstimada: 10011010,
+        magnitude: 3.81, dimensoes: 232.323, urlImg: 'exemplimg', raio: 110.0
+      }, {
+        id: 1, nome: 'Andromeda3333333333', constelacao: 'Andromeda',
+        declinacao: '+41° 16′ 9″', distancia: '2.537.000',
+        desvioVermelho: '−0,001', rotacao: 240.000, massa: 1.222,
+        coordenadas: '233mk23', descricao: '2e32mmad,ceekkr', estrelas: 2, IdadeEstimada: 10011010,
+        magnitude: 3.81, dimensoes: 232.323, urlImg: 'exemplimg', raio: 110.0
+      }
+      
+    ];
+  }
+}
