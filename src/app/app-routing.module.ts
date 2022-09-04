7df7fc6a7eea57688fefd49545661cb92f85831d
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-09-04 08:14:00 -0300

    Definido rota de componente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BlackHoleComponent } from './components/black-hole/black-hole.component';
+import { DayPhotoComponent } from './components/day-photo/day-photo.component';
 import { GalaryGalaxyComponent } from './components/galary-galaxy/galary-galaxy.component';
 import { GalaxyListComponent } from './components/galaxy-list/galaxy-list.component';
 import { HomeComponent } from './components/home/home.component';
@@ -23,8 +24,8 @@ const routes: Routes = [
         component:GalaryGalaxyComponent
       },
       {
-        path:'foto-day',
-        component:GalaryGalaxyComponent
+        path:'fotoDay',
+        component:DayPhotoComponent
       },
       {
         path:'blackHole',
