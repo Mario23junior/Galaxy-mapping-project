911d24961303ff4fd6fe9d7bd82ec514430743fe
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-09-05 02:10:42 -0300

    Criando service de envio de data

---
+++ src/app/components/service/apod-day-photo.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { DayPhotoNasa } from 'src/app/model/DayPhotoNasaBridey';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApodDayPhotoService {
+
+  private baseUrl = `http://localhost:8080`
+  private readonly API = `/project/api/photo/date=`
+
+  constructor(private httpClient: HttpClient) { }
+
+  findByDate(date: any): Observable<DayPhotoNasa> {
+    return this.httpClient.get<DayPhotoNasa>(`${this.API}${date}`)
+  }
+}
